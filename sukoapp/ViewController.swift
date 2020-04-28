49cc2f7d41c264676c141bc29cab9413c0cb8b64
cxvc <cxvc>
2020-04-29 00:35:53 +0700

    Initial Commit

---
+++ sukoapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sukoapp
+//
+//  Created by macbook on 4/29/20.
+//  Copyright © 2020 SmartCyberSolution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
