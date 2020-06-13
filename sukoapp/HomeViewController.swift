6bd0d53c30a27ed6abaf31c14e4ef02c5814c0d1
Suko Widodo <voodoomodoo@gmail.com>
2020-06-13 23:17:49 +0700

    fix feature

---
+++ sukoapp/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  sukoapp
+//
+//  Created by macbook on 6/13/20.
+//  Copyright © 2020 SmartCyberSolution. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- sukoapp/LoginController.swift
+++ sukoapp/LoginController.swift
@@ -12,19 +12,32 @@ class LoginController: UIViewController {
     @IBOutlet weak var etUsername: UITextField!
     @IBOutlet weak var etPassword: UITextField!
     
-    @IBAction func onClick(_ sender: Any) {
-        
+    @IBAction func onClickOk(_ sender: Any) {
+    
         let username = etUsername.text!
         let password = etPassword.text!
         
         let req = Request()
+        
         req.httpRequest(endpoint: "/login",
                         parameters: ["username" : username, "password" : password],
                         completion: { result in
             switch result {
-                case .success(let message):
-                    print(result)
-                    break
+            case .success(let message):
+                if message.code==200{
+                    DispatchQueue.main.async{
+                    self.performSegue(withIdentifier: "first_segue", sender: message)
+                    }
+                }else{
+                    DispatchQueue.main.async{
+                        let alertController = UIAlertController(title: "Error", message:
+                            message.message, preferredStyle: .alert)
+                        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                        
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+                break
                 case .failed(let err):
                     print(err)
                     break
